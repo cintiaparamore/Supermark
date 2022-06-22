8e8f6164a4fff551866d39263ce4a885c542a01a
cintiaparamore <cintia.paramore@gmail.com>
2022-06-22 14:13:27 -0300

    actualizando class producto

--- src/supermark/code/Producto.java
+++ src/supermark/code/Producto.java
@@ -3,36 +3,23 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-
 public class Producto {
 
 	Statement stmt = null;
 
 	ConexionBDD connection = new ConexionBDD();
-    Connection acceso;
-    
-	
+	Connection acceso;
+
 	private int codigo;
 	private String nombre;
 	private String marca;
 	private String descripcion;
-<<<<<<< HEAD
-	private Date fecha_vec;
-	private Float precio;
-	private Integer stock;
-	private String categoria;
-
-	public Producto(Integer id, String nombre, String marca, String descripcion, Date fecha_vec, Float precio,
-=======
 	private Double precio;
 	private Integer stock;
 	private String categoria;
 
-			
-	
-	public Producto(int codigo, String nombre, String marca, String descripcion, Double precio,
->>>>>>> 4011de41b1665291715637520b03e716c6838a49
-			Integer stock, String categoria) {
+	public Producto(int codigo, String nombre, String marca, String descripcion, Double precio, Integer stock,
+			String categoria) {
 		super();
 		this.codigo = codigo;
 		this.nombre = nombre;
@@ -43,20 +30,12 @@ public class Producto {
 		this.categoria = categoria;
 	}
 
-<<<<<<< HEAD
-	public Integer getId() {
-		return id;
-=======
-	
 	public Producto() {
 		super();
 	}
-	
-		
 
 	public Integer getcodigo() {
 		return codigo;
->>>>>>> 4011de41b1665291715637520b03e716c6838a49
 	}
 
 	public void setcodigo(Integer codigo) {
@@ -79,24 +58,11 @@ public class Producto {
 		this.marca = marca;
 	}
 
-<<<<<<< HEAD
-	public Date getFecha_vec() {
-		return fecha_vec;
-	}
-
-	public void setFecha_vec(Date fecha_vec) {
-		this.fecha_vec = fecha_vec;
-	}
-
-	public Float getPrecio() {
-=======
-		
 	public Double getPrecio() {
->>>>>>> 4011de41b1665291715637520b03e716c6838a49
 		return precio;
 	}
 
-	public void setPrecio(Float precio) {
+	public void setPrecio(Double precio) {
 		this.precio = precio;
 	}
 
@@ -123,8 +89,7 @@ public class Producto {
 	public void setCategoria(String categoria) {
 		this.categoria = categoria;
 	}
-	
-	
+
 	public void mostrar() {
 
 		System.out.println("Código: " + this.codigo);
@@ -132,159 +97,135 @@ public class Producto {
 		System.out.println("Marca: " + this.marca);
 		System.out.println("Descripcion: " + this.descripcion);
 		System.out.println("Precio: " + this.precio);
-		System.out.println("Stock: "+this.stock);
-		System.out.println("Categoria: "+this.categoria);
+		System.out.println("Stock: " + this.stock);
+		System.out.println("Categoria: " + this.categoria);
 	}
-<<<<<<< HEAD
-	   	
-}
 
-=======
-		
-		
-	public void agregarProducto(String nombre, String marca, String descripcion, Double precio, int stock, int categoria) {
-		
+	public void agregarProducto(String nombre, String marca, String descripcion, Double precio, int stock,
+			int categoria) {
+
 		acceso = connection.Conectar();
-			
+
 		try {
 			stmt = acceso.createStatement();
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		String sql_insertar = "INSERT INTO productos \r\n"
-				     + "VALUES \r\n"
-				     + "(null,'"+nombre+"','"+marca+"','"+descripcion+"',"+precio+","+stock+","+categoria+")\r\n"
-				     + ";";
-		
-		
-		
+
+		String sql_insertar = "INSERT INTO productos \r\n" + "VALUES \r\n" + "(null,'" + nombre + "','" + marca + "','"
+				+ descripcion + "'," + precio + "," + stock + "," + categoria + ")\r\n" + ";";
+
 		try {
 			stmt = acceso.createStatement();
 		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
-		//ResultSet rs= stmt.executeQuery(sql);
-		
+
+		// ResultSet rs= stmt.executeQuery(sql);
+
 		try {
 			stmt.executeUpdate(sql_insertar);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} 
-		
+		}
+
 		System.out.println("Producto agregado.");
-		
-		
-	
-		
-		
-		
+
 	}
-	
 
 	public void modificarNombreProducto(int codmodi, String nommodi) {
-		
-		String sql_modi = "UPDATE productos SET nombre ='"+nommodi+"' WHERE codigo = "+codmodi;
-		
+
+		String sql_modi = "UPDATE productos SET nombre ='" + nommodi + "' WHERE codigo = " + codmodi;
+
 		try {
 			stmt.executeUpdate(sql_modi);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}  
-		
-		System.out.println("Producto "+codmodi+" modificado.");
-			
+		}
+
+		System.out.println("Producto " + codmodi + " modificado.");
+
 	}
 
-	
-	
-   public void modificarPrecioProducto(int codmodi, Double preciomodi) {
-		
-		String sql_modi = "UPDATE productos SET precio ="+preciomodi+" WHERE codigo = "+codmodi;
-		
+	public void modificarPrecioProducto(int codmodi, Double preciomodi) {
+
+		String sql_modi = "UPDATE productos SET precio =" + preciomodi + " WHERE codigo = " + codmodi;
+
 		try {
 			stmt.executeUpdate(sql_modi);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}  
-		
-		System.out.println("Producto "+codmodi+" modificado.");
-		
-	}
-	
+		}
 
-   
+		System.out.println("Producto " + codmodi + " modificado.");
+
+	}
+
 	public void eliminarProducto(int codeli) {
-		
-		String sql_modi = "DELETE FROM productos WHERE codigo ="+codeli;
-		
+
+		String sql_modi = "DELETE FROM productos WHERE codigo =" + codeli;
+
 		try {
 			stmt.executeUpdate(sql_modi);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}  
-		
-		System.out.println("Producto "+codeli+" eliminado.");
-			
+		}
+
+		System.out.println("Producto " + codeli + " eliminado.");
+
 	}
-	
-	
-	
- 	public void listarProductos() {
-		
+
+	public void listarProductos() {
+
 		System.out.println("LISTADO DE PRODUCTOS");
-				
+
 		try {
-			
-			String sql="Select * from productos";
-			
+
+			String sql = "Select * from productos";
+
 			acceso = connection.Conectar();
 			stmt = acceso.createStatement();
-			
-			ResultSet rs= stmt.executeQuery(sql);
-			
-			int c=0;
-			
+
+			ResultSet rs = stmt.executeQuery(sql);
+
+			int c = 0;
+
 			while (rs.next()) {
 				int codigo = rs.getInt("codigo");
-				String nombre=rs.getString("nombre");
+				String nombre = rs.getString("nombre");
 				String marca = rs.getString("marca");
 				String descripcion = rs.getString("descripcion");
 				Double precio = rs.getDouble("precio");
-				
+
 				System.out.print("Código: " + codigo + " | ");
 				System.out.print("Nombre: " + nombre + " | ");
-				System.out.print("Marca: " + marca+ " | ");
-				System.out.print("Descripción: " + descripcion+ " | ");
+				System.out.print("Marca: " + marca + " | ");
+				System.out.print("Descripción: " + descripcion + " | ");
 				System.out.println("Precio: " + precio);
 				c++;
 			}
-			
-			System.out.println("Cantidad de Productos: "+c);
-									
+
+			System.out.println("Cantidad de Productos: " + c);
+
 		} catch (SQLException e1) {
 			// TODO Auto-generated catch block
 			System.out.println("Error al conectarse a la Base de Datos.");
 			e1.printStackTrace();
 		}
 	}
-	
-	
-	
+
 	public void listarUnProducto(int codigoBuscado) {
-		
+
 		System.out.println("------- PRODUCTO SELECCIONADO -------");
-		
-		String sql = "SELECT * FROM productos WHERE codigo="+codigoBuscado;
-		
+
+		String sql = "SELECT * FROM productos WHERE codigo=" + codigoBuscado;
+
 		acceso = connection.Conectar();
 		try {
 			stmt = acceso.createStatement();
@@ -292,31 +233,31 @@ public class Producto {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
-		
+
 		try {
 			ResultSet rs;
-					 
+
 			rs = stmt.executeQuery(sql);
 			rs.next();
 			int codigo = rs.getInt("codigo");
-	    	String nombre=rs.getString("nombre");
+			String nombre = rs.getString("nombre");
 			String marca = rs.getString("marca");
 			String descripcion = rs.getString("descripcion");
 			Double precio = rs.getDouble("precio");
-					
+
 			System.out.print("Código: " + codigo + " | ");
 			System.out.print("Nombre: " + nombre + " | ");
-			System.out.print("Marca: " + marca+ " | ");
-			System.out.print("Descripción: " + descripcion+ " | ");
+			System.out.print("Marca: " + marca + " | ");
+			System.out.print("Descripción: " + descripcion + " | ");
 			System.out.println("Precio: " + precio);
-			
+
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
 
 }
->>>>>>> 4011de41b1665291715637520b03e716c6838a49
+
+	
