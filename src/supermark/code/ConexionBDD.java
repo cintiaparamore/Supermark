a8a2e67a8703f45251528b4a3ac8c23a62a99aa8
Cintia Veronica Carrasco <107169427+cintiaparamore@users.noreply.github.com>
2022-07-26 21:00:32 -0300

    Update ConexionBDD.java

--- src/supermark/code/ConexionBDD.java
+++ src/supermark/code/ConexionBDD.java
@@ -12,7 +12,7 @@ public class ConexionBDD {
 	static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
 	static final String DB_URL = "jdbc:mysql://localhost:3306/bd_supermark";
 	static final String USER = "root";
-	static final String PASS = "blackpink94";
+	static final String PASS = "";
 
 	private	Connection connection = null;
 //	private	Statement stmt = null;
